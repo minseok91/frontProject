aad2a11ce7550113b7d3bdf70effcded51d2b5e0
김동주 <kimmallangcp@gmail.com>
2022-04-19 23:01:19 +0900

    [게시판 조회] UI 구현 및 스타일 적용

--- src/components/main/board/BoardView.js
+++ src/components/main/board/BoardView.js
@@ -2,6 +2,8 @@ function BoardView() {
     return (
         <div className="board-view">
             <h3>게시판 조회</h3>
+            <div className="title">제목입니다.</div>
+            <div className="contents">내용입니다.</div>
         </div>
     );
 }
